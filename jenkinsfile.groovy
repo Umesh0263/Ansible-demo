709f0347e99a9244bf942662156db71d3e2c35c7
Umesh0263 <145130109+Umesh0263@users.noreply.github.com>
2024-05-23 11:53:55 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,28 +1,20 @@
 pipeline {
     agent any
+
     stages {
-        stage('pull the source code from git') {
+        stage('Pull the source code from github') {
             steps {
-               git branch: 'main', url: 'https://github.com/Umesh0263/Ansible-demo.git'
-'''
-                
+                git branch: 'main', url: 'https://github.com/Umesh0263/Ansible-demo.git'
             }
         }
-        stage('install apache package') {
+        stage('install apache') {
             steps {
                 sh '''sudo apt update
                 sudo apt install apache2 -y
-                sudo systemctl start apache2
+                sudo systemctl start apache2 
                 sudo systemctl enable apache2
-
-'''
-            }
-        }
-        stage('Deploy') {
-            steps {
-                sh '''sudo cp -r index.html /var/www/html/index.html'''
-
-'''
+                sudo cp -r index.html /var/www/html/index.html
+                sudo chmod 644 /var/www/html/index.html'''
             }
         }
     }
