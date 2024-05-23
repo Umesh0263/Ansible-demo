697535775c8a973a914b8ae8a74e99a9efc72fe8
Umesh0263 <145130109+Umesh0263@users.noreply.github.com>
2024-05-23 12:22:53 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,17 +4,17 @@ pipeline {
     stages {
         stage('Pull the source code from github') {
             steps {
-                git branch: 'main', url: 'https://github.com/Umesh0263/Ansible-demo.git'
+                sh 'sudo echo "This is first stage"'
             }
         }
         stage('install apache') {
             steps {
-                sh '''sudo apt update
-                sudo apt install apache2 -y
-                sudo systemctl start apache2 
-                sudo systemctl enable apache2
-                sudo cp -r index.html /var/www/html/index.html
-                sudo chmod 644 /var/www/html/index.html'''
+               sh '''sudo apt update
+               sudo apt install apache2 -y
+               sudo systemctl start apache2 
+               sudo systemctl enable apache2
+               sudo echo "<h1>Welcome To Jenkins Project</h1>" | sudo tee /var/www/html/index.html
+               sudo chmod 644 /var/www/html/index.html '''
             }
         }
     }
